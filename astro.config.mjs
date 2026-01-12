7ffe6b50a57e841d5f53679f5f4c9acf1edb34cd
Shavar Sean Cox <shavarsean@gmail.com>
2026-01-12 02:05:37 -0500

    re-enable server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
